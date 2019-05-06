84557caff5265b2781d024f064db890f6e9bcf9c
daquexian <daquexian566@gmail.com>
2019-05-06 22:04:47 +0800

    Attempt to use cmake 3.5

--- ci/build_onnx2bnn.sh
+++ ci/build_onnx2bnn.sh
@@ -2,6 +2,7 @@
 set -e
 
 mkdir build_onnx2bnn && cd build_onnx2bnn
-cmake ..
-cmake --build . -- -j$(nproc)
+# azure pipeline image aliases cmake to their cmake 3.12
+/usr/bin/cmake ..
+/usr/bin/cmake --build . -- -j$(nproc)
 cd -
