d7f431e0a09f4b0a3acfa04ec894065b0c50d04b
daquexian <daquexian566@gmail.com>
2019-05-17 18:27:43 +0800

    Update run.cpp for multiple blobs

--- binaries/run.cpp
+++ binaries/run.cpp
@@ -33,45 +33,24 @@ int main(int argc, char **argv) {
         net1->run(input);
     }
     const auto t2 = Clock::now();
-    css blob_name = argv[2];
-    LOG(INFO) << "Fetching blob: " << blob_name;
-    const auto &blob1 = net1->get_blob(blob_name);
-    LOG(INFO) << blob1->total();
-    if (blob1->data_type == bnn::DataType::Float) {
-        blob1->dump("/data/local/tmp/mat.txt");
-    }
-    FORZ(i, std::min(static_cast<int>(blob1->total()), 10)) {
+
+    for (int i = 2; i < cmdl.size(); i++) {
+        css blob_name = argv[i];
+        LOG(INFO) << "Fetching blob: " << blob_name;
+        const auto &blob1 = net1->get_blob(blob_name);
+        LOG(INFO) << static_cast<float *>(blob1->data)[0];
         if (blob1->data_type == bnn::DataType::Float) {
-            LOG(INFO) << static_cast<float *>(blob1->data)[i];
-        } else {
-            LOG(INFO) << binrep(static_cast<uint64_t *>(blob1->data) + i, 64, false);
+            blob1->dump("/data/local/tmp/mat_" + blob_name + ".txt");
+        }
+        FORZ(j, std::min(static_cast<int>(blob1->total()), 10)) {
+            if (blob1->data_type == bnn::DataType::Float) {
+                LOG(INFO) << blob_name << ": " << static_cast<float *>(blob1->data)[j];
+            } else {
+                LOG(INFO) << blob_name << ": " << binrep(static_cast<uint64_t *>(blob1->data) + j, 64, true);
+            }
         }
     }
-    LOG(INFO) << "Time: "
-              << 1.f *
-                     std::chrono::duration_cast<std::chrono::nanoseconds>(t2 -
-                                                                          t1)
-                         .count() /
-                     N / 1000000000;
 #ifdef BNN_BENCHMARK
     net1->print_time();
 #endif
-
-    /*
-    bnn::Net net2;
-    net2.model_ = model;
-    net2.prepare();
-    LOG(INFO) << "-----";
-    net2.optimize = false;
-
-    net2.run(input);
-    const auto &blob2 = net2.get_blob(blob_name);
-    LOG(INFO) << blob2->total();
-    FORZ(i, std::min(static_cast<int>(blob2->total()), 10)) {
-        LOG(INFO) << static_cast<float *>(blob2->data)[i];
-    }
-
-    const bool eq = (*blob1 == *blob2);
-    BNN_ASSERT(eq, "");
-    */
 }
