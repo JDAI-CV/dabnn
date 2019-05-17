cd989083019aa9f8f99c561fbcaf73da45fa1790
daquexian <daquexian566@gmail.com>
2019-05-17 18:21:23 +0800

    Add Pad operation (however it is not used)

---
+++ dabnn/layers/Pad.cpp
@@ -0,0 +1,14 @@
+// Copyright 2019 JD.com Inc. JD AI
+
+#include "Pad.h"
+
+#include <dabnn/pad.h>
+#include <dabnn/net.h>
+
+namespace bnn {
+void Pad::forward_impl() const { 
+    pad(*input_mat, pad_h, pad_w, *output_mat);
+}
+
+}  // namespace bnn
+
