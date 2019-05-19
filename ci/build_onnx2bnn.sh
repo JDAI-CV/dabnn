ecc5be153f650058772b33805ca40660b18ca3b3
daquexian <daquexian566@gmail.com>
2019-05-19 12:29:44 +0800

    Set min cmake version to 3.9

--- ci/build_onnx2bnn.sh
+++ ci/build_onnx2bnn.sh
@@ -2,7 +2,6 @@
 set -e
 
 mkdir build_onnx2bnn && cd build_onnx2bnn
-# azure pipeline image aliases cmake to their cmake 3.12
-/usr/bin/cmake ..
-/usr/bin/cmake --build . -- -j$(nproc)
+cmake ..
+cmake --build . -- -j$(nproc)
 cd -
